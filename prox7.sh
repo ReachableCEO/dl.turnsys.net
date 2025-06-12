7e210ef5875ddb0f0d2d56cb52fb178d14e81a0f
ReachableCEO <charles@turnsys.com>
2025-06-12 11:41:40 -0500

    goodbye...

--- prox7.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm -f /etc/apt/sources.list.d/*
-echo "deb http://download.proxmox.com/debian/pve bookworm pve-no-subscription" > /etc/apt/sources.list.d/pve-install-repo.list
-wget http://download.proxmox.com/debian/proxmox-release-bookworm.gpg -O /etc/apt/trusted.gpg.d/proxmox-release-bookworm.gpg
-apt update && apt -y full-upgrade
-apt-get -y install ifupdown2 ipmitool ethtool net-tools lshw
-
-#curl -s http://dl.turnsys.net/newSrv.sh|/bin/bash
